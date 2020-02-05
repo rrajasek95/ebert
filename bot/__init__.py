516c8313da7ce3c33fdd115bd50bdd3fdca6be57
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2020-02-05 01:31:16 -0800

    Fix lint issue

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_executor import Executor
 
 from .bot import bp
+
 def create_app():
     app = Flask(__name__)
 
-    app.register_blueprint(bot.bp)
+    app.register_blueprint(bp)
 
     app.config['EXECUTOR'] = Executor(app)
     app.config['EXECUTOR_PROPAGATE_EXCEPTIONS'] = True 
