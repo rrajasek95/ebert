61330f472786f40dde062dad742693cf2181725c
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2020-02-06 03:19:59 -0800

    Add basic config for ElasticSearch

--- bot/message.py
+++ bot/message.py
@@ -1,4 +1,3 @@
-import os
 import requests
 import json
 
@@ -15,10 +14,10 @@ def generate_echo_message(sender_ps_id, message):
 
 
 def call_send_api(request, app):
-    url = "https://graph.facebook.com/v6.0/me/messages?access_token=%s" % os.getenv('PAGE_ACCESS_TOKEN', '')
-    app.logger.info("POST to URL : %s" % url)
+
+    app.logger.info("POST to URL : %s" % app.config['GRAPH_MESSAGES_ENDPOINT'])
     app.logger.info("Payload: %s" % json.dumps(request))
 
-    r = requests.post(url, json=request)
+    r = requests.post(app.config['GRAPH_MESSAGES_ENDPOINT'], json=request)
     app.logger.info(r.status_code)
     app.logger.info(r.text)
